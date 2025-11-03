dd73bd85fba894d31969f5494b83a3c07a5f3504
Jeighost <prodjeighost@gmail.com>
2025-11-03 09:47:18 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -87,3 +87,9 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url || '/')
+  );
+});
